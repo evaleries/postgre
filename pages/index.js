b89563acc58ac696b8b4fd3c75e7dfb3fca181bd
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2021-07-30 13:56:27 +0700

    project init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Postgre</title>
+      </Head>
+
+      <main className="flex flex-col items-center justify-center min-h-screen">
+        <h1>Postgre</h1>
+      </main>
+    </div>
+  );
+}
